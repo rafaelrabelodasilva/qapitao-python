662d2e3dddd7f94d3793d26710ee76a444093dcc
rafaelrabelodasilva <rafaelrabelodasilva@outlook.com>
2025-12-06 10:00:45 -0300

    Tipos de Conta (Herança)

--- banco.py
+++ banco.py
@@ -11,10 +11,31 @@ class ContaBancaria:
     def consulta_saldo(self):
         print(f'Saldo atual de {self.titular}: R$ {self.saldo}')
 
+class ContaCorrente(ContaBancaria):
+    def sacar(self, valor):
+        taxa = 2
+        total = valor + taxa
 
-fernando = ContaBancaria('Fernando', 1000)
-ana = ContaBancaria('Ana', 2000)
+        if(total > self.saldo):
+            print('Saldo insuficiente para saque.')
+        else:
+          self.saldo = self.saldo - total
+          print(f'Saque de R$ {valor} realizado com sucesso. Taxa de R$ {taxa} aplicada.')
 
+class ContaPoupanca(ContaBancaria):
+    def sacar(self, valor):
+        if(valor > self.saldo):
+            print('Saldo insuficiente para saque.')
+        else:
+          self.saldo = self.saldo - valor
+          print(f'Saque de R$ {valor} realizado com sucesso.')
+
+
+fernando = ContaCorrente('Fernando', 1000)
+ana = ContaPoupanca('Ana', 2000)
+
+fernando.sacar(500)
 fernando.consulta_saldo()
-fernando.depositar(500)
-fernando.consulta_saldo()
+
+ana.sacar(100)
+ana.consulta_saldo()
