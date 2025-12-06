73b03d3eee949b1367665e68e8307e4d01ef1669
rafaelrabelodasilva <rafaelrabelodasilva@outlook.com>
2025-12-06 09:49:31 -0300

    "Ações" da Classe (Métodos)

--- banco.py
+++ banco.py
@@ -1,11 +1,20 @@
 class ContaBancaria:
-    # Construtor: que é executado automaticamente quando eu ativar uma nova instância
+    # Construtor: que é executado automaticamente quando eu ativar uma nova instancia
     def __init__(self, titular, saldo):
         self.titular = titular
         self.saldo = saldo
+
+    def depositar(self, valor):
+        self.saldo = self.saldo + valor
+        print(f'Depósito de R$ {valor} realizado com sucesso.')
+
+    def consulta_saldo(self):
+        print(f'Saldo atual de {self.titular}: R$ {self.saldo}')
 
 
 fernando = ContaBancaria('Fernando', 1000)
 ana = ContaBancaria('Ana', 2000)
 
-print(fernando.titular)
+fernando.consulta_saldo()
+fernando.depositar(500)
+fernando.consulta_saldo()
