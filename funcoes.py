b686fc247b945255136b973aa6fbd69e62ea84db
rafaelrabelodasilva <rafaelrabelodasilva@outlook.com>
2025-12-06 09:25:27 -0300

    A Diferença entre "Mostrar" e "Entregar" (O comando return)

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,13 @@
-def mostra_nome(nome):
-    print(nome)
+# def mostra_nome(nome):
+#     print(nome)
 
-mostra_nome('Rafael')
+# mostra_nome('Rafael')
+
+def converter_para_real(valor_em_dolar):
+    cotacao = 5.50
+    resultado = valor_em_dolar * cotacao
+    return resultado
+
+valor_convertido = converter_para_real(100)
+
+print(f'O valor convertido é R$ {valor_convertido} ')
